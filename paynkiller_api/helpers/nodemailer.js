8e569dc8181177cc946f219d502fcba56a85c092
AndhikaJeremia <andhika.jeremia@gmail.com>
2021-03-17 20:58:28 +0700

    PP4-11 ForgotPass Fix Error

--- paynkiller_api/helpers/nodemailer.js
+++ paynkiller_api/helpers/nodemailer.js
@@ -1,11 +1,10 @@
 const nodemailer = require('nodemailer')
 const NODEMAILER = process.env.NODEMAILER
-const USER = process.env.USER
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth : {
-        user: USER,
+        user: 'ezrayamin16@gmail.com',
         pass: NODEMAILER
     },
     tls : {
