fd5fcde60fa4b9c61ad42c88e2e121491b9295f6
LordAvorio <gadingramjets@gmail.com>
2021-03-22 10:44:55 +0700

    PP4-41 Master Produk dan Bahan Baku

--- paynkiller_api/controllers/index.js
+++ paynkiller_api/controllers/index.js
@@ -2,10 +2,24 @@ const userController = require('./userController')
 const orderController = require('./orderController')
 const categoryController = require('./categoryController')
 const produkController = require('./produkController')
+const brandController = require('./brandController')
+const uomController = require('./uomController')
+const adminController = require('./adminController')
+const productController = require('./productController')
+const rawMaterialController = require('./rawMaterialController')
+const stokProdukController = require('./stokProdukController')
+const stokRawMaterialController = require('./stokRawMaterialController')
 
 module.exports = {
     userController,
     categoryController,
     produkController,
-    orderController
+    orderController,
+    brandController,
+    uomController,
+    adminController,
+    productController,
+    rawMaterialController,
+    stokProdukController,
+    stokRawMaterialController
 }
